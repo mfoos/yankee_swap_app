9e5a96ad8c435dff4674c6451c3978fa8a5bec45
Marianna Foos <mfoos@bluebirdbio.com>
2020-05-19 15:46:14 -0400

    Added text boxes for claimants

--- img-module.R
+++ img-module.R
@@ -1,12 +1,14 @@
 img_UI <- function(id) {
   ns <- NS(id)
   tagList(
-    imageOutput(
-      outputId = ns("img"), 
-      click = clickOpts(id = ns("img_click"), clip = FALSE),
-      #width = 120, 
-      height = 300, 
-      inline = TRUE
+    tags$div(style="display: inline-block;",
+      imageOutput(
+        outputId = ns("img"), 
+        click = clickOpts(id = ns("img_click"), clip = FALSE),
+        width = 300, 
+        height = 225
+      ),
+      textInput(ns("owner"), "", placeholder = "Claim this gift")
     )
   )
 }
@@ -16,8 +18,8 @@ img_server <- function(input, output, session, prefix) {
   click_status <- reactiveValues(show = FALSE)
   
   observeEvent(input$img_click, {
-    #click_status$show <- !click_status$show
-    click_status$show <- TRUE
+    click_status$show <- !click_status$show
+    #click_status$show <- TRUE
   })
   
   output$img <- renderImage({
@@ -28,17 +30,17 @@ img_server <- function(input, output, session, prefix) {
     if (!click_status$show) {
       list(
         src = wrap, 
-        width = 400, 
-        height = 300, 
-        style = "img{object-fit: contain;}",
+        width = 300, 
+        height = 225, 
+        style = "object-fit: contain; margin: 10px;",
         contentType = "image/jpg"
       )
     } else {
       list(
         src = gift, 
-        width = 400, 
-        height = 300, 
-        style = "img{object-fit: contain;}",
+        width = 300, 
+        height = 225, 
+        style = "object-fit: contain; margin: 10px;",
         contentType = "image/png"
       )
     }
